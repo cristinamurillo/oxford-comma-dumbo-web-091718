a0a82a5309a86f8fce563c602d6f2644c874b4cf
Cristina Murillo <cmurill1@jhu.edu>
2018-08-14 22:47:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-  if (array.size>2)
-  last_part = array.pop
-  first_part = array.join(", ")
-  first_part << ", and #{last_part}"
+  if array.size>2
+    last_part = array.pop
+    first_part = array.join(", ")
+    first_part << ", and #{last_part}"
+ elsif array.size == 2
+    
+ end
 end
